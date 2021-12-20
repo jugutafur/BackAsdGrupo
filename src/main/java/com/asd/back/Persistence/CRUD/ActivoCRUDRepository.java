4d217141ffc4fcaa579d23bd399f5525d240f95e
DESKTOP-8GLU08G\HP <juantafur.25@gmail.com>
2021-12-20 06:19:31 -0500

    add new features

--- src/main/java/com/asd/back/Persistence/CRUD/ActivoCRUDRepository.java
+++ src/main/java/com/asd/back/Persistence/CRUD/ActivoCRUDRepository.java
@@ -6,5 +6,5 @@ import org.springframework.data.repository.CrudRepository;
 import java.util.List;
 
 public interface ActivoCRUDRepository extends CrudRepository<Activo, Integer> {
-    List<Activo> findByType(String type);
+    //List<Activo> findByType(String type);
 }
