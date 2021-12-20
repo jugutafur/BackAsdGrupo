ccb3e470781bcd610644021389f3989f0e7cfc5f
DESKTOP-8GLU08G\HP <juantafur.25@gmail.com>
2021-12-20 09:03:27 -0500

    add new features

--- src/main/java/com/asd/back/Persistence/CRUD/ActivoCRUDRepository.java
+++ src/main/java/com/asd/back/Persistence/CRUD/ActivoCRUDRepository.java
@@ -8,5 +8,5 @@ import java.util.List;
 import java.util.Optional;
 
 public interface ActivoCRUDRepository extends CrudRepository<Activo, Integer> {
-    //Optional<List<Active>> findByType(int type);
+    List<Active> findByType(int type);
 }
